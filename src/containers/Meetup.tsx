73c40476e20de81319167e91eadfd4f342d9a2f0
Jonas Spieler <jonas@spieler.io>
2019-09-28 23:53:33 +0200

    Change Meetup Layout

--- src/containers/Meetup.tsx
+++ src/containers/Meetup.tsx
@@ -27,6 +27,58 @@ const GET_MEETUP = gql`
   }
 `;
 
+const Input = styled.input `
+  display: block;
+  outline: 0;
+  border-width: 0 0 2px;
+  border-color: #3F3F3F;
+  text-indent: 10px; 
+  font-size: 1.5em;
+  width: 50%;
+  height: 2em;
+`;
+
+const Label = styled.label `
+  display: block;
+  font-size: 1.5rem;
+  margin-bottom: 1em;
+`;
+
+const Button = styled.button `
+  margin-top: 0.5rem;
+  font-size: 1rem;
+  background-color: #34ace0;
+  color: #ffff;
+  border: none;
+  padding: 1.2rem;
+  border-radius: 4px;
+  &:hover{
+    cursor: pointer;
+    background-color: #277FA8;
+  }
+  &:active{
+    background-color: #227093;
+  }
+`;
+
+const OuterDiv = styled.div`
+  border-top: 1px solid black;
+  padding: 1rem;
+`;
+
+const InnerDiv1 = styled.div`
+  
+`;
+
+const InnerDiv2 = styled.div`
+  padding-top: 0.5rem;
+`;
+
+const Events = styled.div`
+  font-size: 2em;
+  padding-bottom: 0.5rem;
+`;
+
 const MeetupContainer = styled.div`
   height: 100%;
 `;
@@ -73,23 +125,27 @@ const Meetup: React.SFC<Props> = React.memo(
 
               return (
                 <div>
-                  <h1>{meetup.title}</h1>
-                  Users:
-                  <ul>
-                    {meetup.users.map((user: any, index: number) => (
-                      <li key={index}>{user.name}</li>
-                    ))}
-                  </ul>
-                  Event recommendations:
-                  <div>
+                  <OuterDiv>
+                    <h1>{meetup.title}</h1>
+                    Users:
+                    <ul>
+                      {meetup.users.map((user: any, index: number) => (
+                        <li key={index}>{user.name}</li>
+                      ))}
+                    </ul>
+                  </OuterDiv>
+                  <Events>
+                    Event recommendations:
+                  </Events>
+                  <OuterDiv>
                     {meetup.events.map((event: any, index: number) => (
                       <div key={index}>
                         <h2>{event.title}</h2>
-                        <div>Rating: {event.rating}</div>
-                        <div>PriceLevel: {event.priceLevel}</div>
+                        <InnerDiv1>Rating: {event.rating}</InnerDiv1>
+                        <InnerDiv2>PriceLevel: {event.priceLevel}</InnerDiv2>
                       </div>
                     ))}
-                  </div>
+                  </OuterDiv>
                 </div>
               );
             }}
