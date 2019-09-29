b233860fd981247cd6406e6f200db11c5abffdce
David Schmid <davethehacker@users.noreply.github.com>
2019-09-29 07:50:21 +0200

    frontpage form and transport button side by side

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import ApolloClient from "apollo-boost";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { gql } from "apollo-boost";
 import { getBackURI } from "../utilities/helpers";
 import { push } from "connected-react-router";
@@ -9,25 +9,25 @@ import { AnyAction } from "redux";
 import { ThunkDispatch } from "redux-thunk";
 import { FaSubway, FaCar } from 'react-icons/fa';
 
-const Input = styled.input `
+const Input = styled.input`
   display: block;
   outline: 0;
   border-width: 0 0 2px;
   border-color: #3F3F3F;
   text-indent: 10px; 
   font-size: 1.5em;
-  width: 50%;
+  width: 100%;
   height: 2em;
 
 `;
 
-const Label = styled.label `
+const Label = styled.label`
   display: block;
   font-size: 1.5rem;
   margin-bottom: 1em;
 `;
 
-const Button = styled.button `
+const Button = styled.button`
   margin-top: 0.5rem;
   font-size: 1rem;
   background-color: #34ace0;
@@ -44,63 +44,69 @@ const Button = styled.button `
   }
 `;
 
-const client = new ApolloClient({uri: getBackURI()})
+const client = new ApolloClient({ uri: getBackURI() })
 
 interface IState {
-    curLoc?: string;
-    curDate?: string;
-    travelTime?: string;
-    arrivalTime?: string
+  curLoc?: string;
+  curDate?: string;
+  travelTime?: string;
+  arrivalTime?: string
 }
-interface IProps{
-  selected:string;
+interface IProps {
+  selected: string;
   dispatch: ThunkDispatch<void, void, AnyAction>;
 }
 class Form extends React.Component<IProps, IState> {
-    constructor(props : any) {
-        super(props);
-        this.state = {
-            curLoc: "",
-            curDate: "",
-            travelTime: "",
-            arrivalTime: ""
-        }
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      curLoc: "",
+      curDate: "",
+      travelTime: "",
+      arrivalTime: ""
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleInputChange(event : any) {
-        this.setState({[event.target.name]: event.target.value})
-    }
+  handleInputChange(event: any) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-  handleSubmit(event:any){
+  handleSubmit(event: any) {
     // travel time in seconds
     let travelMinutes = +this.state.travelTime * 60;
     let arrivalDate = new Date(this.state.curDate + " " + this.state.arrivalTime)
 
     this.props.dispatch(push("/groupInfo"));
-  
+
     event.preventDefault();
   }
 
   render() {
     return (
       <div style={
-        {marginTop: "2rem"}
+        { marginTop: "2rem" }
       }>
-        <div>
-          
-          <Input type="text" name="curLoc" id="curLoc"
-            value={
-              this.state.curLoc
-            }
-            onChange={
-              this.handleInputChange
-            }/>
-            <Label htmlFor="curLoc">Current Location</Label>
-        </div>
+        <div style={
+          {
+            width: "50%",
+            float: "left"
+          }
+        }>
           <div>
-            
+
+            <Input type="text" name="curLoc" id="curLoc"
+              value={
+                this.state.curLoc
+              }
+              onChange={
+                this.handleInputChange
+              } />
+            <Label htmlFor="curLoc">Current Location</Label>
+          </div>
+          <div>
+
             <Input type="date"
               value={
                 this.state.curDate
@@ -109,11 +115,11 @@ class Form extends React.Component<IProps, IState> {
               id="curDate"
               onChange={
                 this.handleInputChange
-              }/>
-              <Label htmlFor="curDate">Arrival Date</Label>
+              } />
+            <Label htmlFor="curDate">Arrival Date</Label>
           </div>
           <div>
-            
+
             <Input type="time"
               value={
                 this.state.arrivalTime
@@ -122,11 +128,11 @@ class Form extends React.Component<IProps, IState> {
               id="arrivalTime"
               onChange={
                 this.handleInputChange
-              }/>
-              <Label htmlFor="arrivalTime">Arrival Time</Label>
+              } />
+            <Label htmlFor="arrivalTime">Arrival Time</Label>
           </div>
           <div>
-            
+
             <Input type="text"
               value={
                 this.state.travelTime
@@ -135,8 +141,8 @@ class Form extends React.Component<IProps, IState> {
               id="travelTime"
               onChange={
                 this.handleInputChange
-              }/>
-              <Label htmlFor="travelTime">Max travel time (min)</Label>
+              } />
+            <Label htmlFor="travelTime">Max travel time (min)</Label>
           </div>
           <Button
             type="submit"
@@ -144,17 +150,24 @@ class Form extends React.Component<IProps, IState> {
           >View Single</Button>
           <Button
             type="submit"
-            style={{marginLeft: "1rem"}}
+            style={{ marginLeft: "1rem" }}
             onClick={this.handleSubmit}
           >View Single</Button>
+
+        </div>
+        <div style={
+          {
+            marginLeft: "50%",
+            width: "100%"
+          }
+        }>
           <div>
-            <div>
-              <FaSubway />
-            </div>
-            <div>
-              <FaCar />
-            </div>
+            <FaSubway size={120} style={{ maxHeight: "80%",  }} />
           </div>
+          <div>
+            <FaCar size={120} style={{ maxHeight: "80%" }} />
+          </div>
+        </div>
       </div>
     )
   }
